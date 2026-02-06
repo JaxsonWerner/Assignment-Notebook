96588231129e00f2b96f42773e783594a0f2efdd
Jaxson <Werner>
2026-02-06 10:37:07 -0600

    base app done

---
+++ Assignment Notebook/AddAssignmentIView.swift
@@ -0,0 +1,16 @@
+//
+//  AddAssignmentItem.swift
+//  Assignment Notebook
+//
+//  Created by Jaxson Werner on 2/4/26.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    @Environment(assignmentItems.self) var
+}
+
+#Preview {
+    AddAssignmentItem()
+}
---
+++ Assignment Notebook/AssignmentItem.swift
@@ -0,0 +1,8 @@
+//
+//  AssignmentItem.swift
+//  Assignment Notebook
+//
+//  Created by Jaxson Werner on 2/4/26.
+//
+
+import Foundation
